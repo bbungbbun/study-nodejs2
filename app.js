e2dc965a470a5291b1cef15f8c09adcb19c40d42
ynnotun <flexible.yuni@gmail.com>
2022-12-26 12:26:32 +0900

    Express - 정적파일을 서비스하는 법

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 app.get('/',function(req, res){
     res.send('hello home page');
+});
+app.get('/route',function(req, res){
+    res.send('hello router, <img src="/cat.jpg">');
 });
 app.get('/login',function(req, res){
     res.send('login please');
 });
 app.listen(3000, function(){
-    console.log('connected 3000 port!')
-})
+    console.log('connected 3000 port!');
+});
