d481e0ab8b03f5bbc6a1ce58c5a687eb2abfd39c
ynnotun <flexible.yuni@gmail.com>
2022-12-26 17:18:47 +0900

    웹앱 제작 6 : 코드의 개선

--- app_file.js
+++ app_file.js
@@ -6,9 +6,43 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.locals.pretty = true; // jade로 작성한 html 줄바꿈
 app.set('views', './views_file');
 app.set('view engine', 'jade');
-app.get('/topic/new',function(req,res){
-    res.render('new');
-});
+// app.get('/upload', function(req, res){
+//     res.render('upload');
+//   });
+//   app.post('/upload', upload.single('userfile'), function(req, res){
+//     res.send('Uploaded : '+req.file.filename);
+//   });
+app.get('/topic/new', function(req, res){
+    fs.readdir('data', function(err, files){
+      if(err){
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+      }
+      res.render('new', {topics:files});
+    });
+  });
+  app.get(['/topic', '/topic/:id'], function(req, res){
+    fs.readdir('data', function(err, files){
+      if(err){
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+      }
+      var id = req.params.id;
+      if(id){
+        // id값이 있을 때
+        fs.readFile('data/'+id, 'utf8', function(err, data){
+          if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+          }
+          res.render('view', {topics:files, title:id, description:data});
+        })
+      } else {
+        // id 값이 없을 때
+        res.render('view', {topics:files, title:'Welcome', description:'Hello, JavaScript for server.'});
+      }
+    })
+  });
 app.post('/topic', function(req,res){
     let title = req.body.title;
     let description = req.body.description
@@ -17,7 +51,7 @@ app.post('/topic', function(req,res){
             console.log(err)
             res.status(500).send('Internal Server Error');
         }
-        res.send('Success!');
+        res.redirect('/topic/'+title);
     })
 })
 app.listen(3000, function(){
