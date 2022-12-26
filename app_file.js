f2b15e38e2331bffe562fe42d130dcb28a739bf7
ynnotun <flexible.yuni@gmail.com>
2022-12-26 16:37:14 +0900

    웹앱 제작 3 : 본문저장

--- app_file.js
+++ app_file.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const fs = require('fs')
+var bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
 app.locals.pretty = true; // jade로 작성한 html 줄바꿈
 app.set('views', './views_file');
 app.set('view engine', 'jade');
@@ -7,7 +10,15 @@ app.get('/topic/new',function(req,res){
     res.render('new');
 });
 app.post('/topic', function(req,res){
-    res.send('hi post');
+    let title = req.body.title;
+    let description = req.body.description
+    fs.writeFile('data/'+title, description, function(err){
+        if(err){
+            console.log(err)
+            res.status(500).send('Internal Server Error');
+        }
+        res.send('Success!');
+    })
 })
 app.listen(3000, function(){
     console.log('connected, 3000 port!')
