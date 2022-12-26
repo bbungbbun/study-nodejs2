0273a73f877ad9484103b777347488e9daf967cd
ynnotun <flexible.yuni@gmail.com>
2022-12-26 14:10:27 +0900

    Express - 웹페이지를 표현하는 방법

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ app.get('/',function(req, res){
 app.get('/route',function(req, res){
     res.send('hello router, <img src="/cat.jpg">');
 });
+app.get('/dynamic',function(req, res){
+    const lis = ``
+    for(let i=0; i<5; i++){
+        lis = lis + '<li>coding</li>';
+    }
+    let time = Date();
+    const output = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hello Dynamic!
+    <ul>
+    ${lis}
+    </ul>
+    ${time}
+</body>
+</html>
+    `
+    res.send(output);
+});
 app.get('/login',function(req, res){
     res.send('login please');
 });
